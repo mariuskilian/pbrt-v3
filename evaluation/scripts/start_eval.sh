6615a1090aa2fa2859e48eec40afa6979bd9f9b1
Marius Kilian <mkilian96@gmail.com>
2021-03-09 11:58:56 +0100

    changed numthreads and adjusted #pixelsamples

--- evaluation/scripts/start_eval.sh
+++ evaluation/scripts/start_eval.sh
@@ -26,6 +26,6 @@ done
 # Create base .pbrt file which sets Scene and Accelerator (and extra arguments)
 python3 $SCRIPTS_PATH/python-scripts/create_base_pbrt.py $INTGR $SCENE $ACCEL "$EXTRA_ARGS"
 # Run pbrt and log the results
-./$SRC_PATH/build/Evaluation/pbrt --outfile output/$SCENE-$INTGR-$ACCEL$FILENAME_POSTFIX.exr $SRC_PATH/scenes/$SCENE/eval_base.pbrt | tee $SCENE/$INTGR-$ACCEL$FILENAME_POSTFIX.log
+./$SRC_PATH/build/Evaluation/pbrt --outfile output/$SCENE-$INTGR-$ACCEL$FILENAME_POSTFIX.exr --nthreads 8 $SRC_PATH/scenes/$SCENE/eval_base.pbrt | tee $SCENE/$INTGR-$ACCEL$FILENAME_POSTFIX.log
 # Remove previously created base .pbrt file
 # python3 $SCRIPTS_PATH/python-scripts/remove_base_pbrt.py $SCENE
