5d28b9d56b94ca31e59c5cc9bec07375c1ccee9f
Marius Kilian <mkilian96@gmail.com>
2021-03-12 11:43:39 +0100

    added bvh-bfs script for rel keys

---
+++ evaluation/results/bvh-bfs_vars/run.sh
@@ -0,0 +1,37 @@
+set -e
+
+SCENE=$1
+MIN=$2
+MAX=$3
+STEP=$4
+
+INTGR="path"
+ACCEL="octree"
+RUN="./../../scripts/start_eval.sh $INTGR $SCENE $ACCEL"
+SOURCE=../../..
+PYSCRIPTS=../../scripts/python-scripts
+BUILD=$SOURCE/build/Evaluation
+
+if [ ! -d $SCENE ]
+then
+    mkdir $SCENE
+fi
+
+if [ ! -d "output" ]
+then
+    mkdir output
+fi
+
+COUNT_STATS="-DCOUNT_STATS=False" # Because we want to compare time
+
+cmake -S $SOURCE -B $BUILD $COUNT_STATS "-DREL_KEYS=True"
+make -C $BUILD -j
+$RUN
+
+cmake -S $SOURCE -B $BUILD $COUNT_STATS "-DREL_KEYS=False"
+make -C $BUILD -j
+$RUN
+
+python3 $PYSCRIPTS/normalize_filename_num_digits.py $SCENE
+python3 $PYSCRIPTS/plot_data.py $SCENE prof
+python3 $PYSCRIPTS/plot_data.py $SCENE mem:topology
