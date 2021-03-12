ee816c97eba8471db3648affef8b94a10b5c8007
Marius Kilian <mkilian96@gmail.com>
2021-03-12 11:48:22 +0100

    added pseudo pbrt argument for automatic names

--- evaluation/results/bvh-bfs_vars/run.sh
+++ evaluation/results/bvh-bfs_vars/run.sh
@@ -6,7 +6,7 @@ MAX=$3
 STEP=$4
 
 INTGR="path"
-ACCEL="octree"
+ACCEL="bvh-bfs"
 RUN="./../../scripts/start_eval.sh $INTGR $SCENE $ACCEL"
 SOURCE=../../..
 PYSCRIPTS=../../scripts/python-scripts
@@ -26,11 +26,11 @@ COUNT_STATS="-DCOUNT_STATS=False" # Because we want to compare time
 
 cmake -S $SOURCE -B $BUILD $COUNT_STATS "-DREL_KEYS=True"
 make -C $BUILD -j
-$RUN
+$RUN "string:relkeys=true"
 
 cmake -S $SOURCE -B $BUILD $COUNT_STATS "-DREL_KEYS=False"
 make -C $BUILD -j
-$RUN
+$RUN "string:relkeys=false"
 
 python3 $PYSCRIPTS/normalize_filename_num_digits.py $SCENE
 python3 $PYSCRIPTS/plot_data.py $SCENE prof
