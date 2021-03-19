312dc400f695d26087463f9242ba6f0711231a0f
Marius Kilian <mkilian96@gmail.com>
2021-03-19 15:02:46 +0100

    script to clean build folders

---
+++ evaluation/scripts/clean_build.sh
@@ -0,0 +1,9 @@
+BUILD=../../build
+
+cmake --build $BUILD/Evaluation --target clean
+cmake --build $BUILD/Evaluation-1 --target clean
+cmake --build $BUILD/Evaluation-2 --target clean
+cmake --build $BUILD/Evaluation-3 --target clean
+cmake --build $BUILD/Evaluation-4 --target clean
+cmake --build $BUILD/Evaluation-5 --target clean
+cmake --build $BUILD/Evaluation-6 --target clean
