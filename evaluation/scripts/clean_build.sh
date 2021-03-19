f96cf17b8dc1ec3a66f46603953746e68185f3af
Marius Kilian <mkilian96@gmail.com>
2021-03-19 15:10:02 +0100

    fixed cleanup script

--- evaluation/scripts/clean_build.sh
+++ evaluation/scripts/clean_build.sh
@@ -1,9 +1,11 @@
+set -e
+
 BUILD=../../build
 
-cmake --build $BUILD/Evaluation --target clean
-cmake --build $BUILD/Evaluation-1 --target clean
-cmake --build $BUILD/Evaluation-2 --target clean
-cmake --build $BUILD/Evaluation-3 --target clean
-cmake --build $BUILD/Evaluation-4 --target clean
-cmake --build $BUILD/Evaluation-5 --target clean
-cmake --build $BUILD/Evaluation-6 --target clean
+rm $BUILD/Evaluation/* -r
+rm $BUILD/Evaluation-1/* -r
+rm $BUILD/Evaluation-2/* -r
+rm $BUILD/Evaluation-3/* -r
+rm $BUILD/Evaluation-4/* -r
+rm $BUILD/Evaluation-5/* -r
+rm $BUILD/Evaluation-6/* -r
