54782661412a62b5a9ae8a4eb729eebbd5b84034
Marius Kilian <mkilian96@gmail.com>
2021-04-02 16:26:50 +0200

    yay

--- evaluation/scripts/python-scripts/exr2png.py
+++ evaluation/scripts/python-scripts/exr2png.py
@@ -1,13 +1,15 @@
 import os
 import sys
+import imageio
 import pyexr
+import numpy as np
 from PIL import Image
-
-order={"embree":0, "bvh":10, "bvh-bfs":20, "kdtree":30, "octree":40, "octree-bfs":50}
         
 def exec():
+    filepaths = []
     savepaths = []
     images = []
+    maxvalues = []
     # Prep variable
     filelist = os.listdir("output")
     for file in filelist:
@@ -16,9 +18,14 @@ def exec():
             savepath = sys.argv[0][:-len("exr2png.py")] + "../../plots/renders/"
             image = pyexr.open(filepath).get()
             images.append(image)
+            maxvalues.append(np.max(image))
+            filepaths.append(filepath)
             savepaths.append(savepath + file[:-3] + "png")
-    for i in range(len(savepaths)):
-        im = Image.fromarray(images[i])
-        im.save(savepaths[i])
+    for i in range(len(images)):
+        #images[i] *= 16 / maxvalues[i]
+        images[i] = np.where(images[i]<=0.0031308,12.92 * images[i], 1.055*(images[i]**(1/2.4)) - 0.055)
+        images[i] = np.clip(images[i], 0, 1)
+        images[i] = (images[i] * 255).astype(np.uint8)
+        Image.fromarray(images[i]).save(savepaths[i])
     
 exec()
